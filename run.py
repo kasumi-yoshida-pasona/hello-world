59ab7b788bfc45f060b9f54048e616e1ce87cd55
kasumi-yoshida-pasona <110513233+kasumi-yoshida-pasona@users.noreply.github.com>
2023-01-29 21:46:11 +0900

    ルーティング

--- run.py
+++ run.py
@@ -3,7 +3,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello World!"
-    
+  return "Hello World!"
+
+@app.route('/flask')
+def hello_sample():
+  return "Hello flask."
+
+@app.route('/user/<user_id>')
+def hello_person(user_id):
+  return "Hello " + user_id
+
 if __name__ == '__main__':
-    app.run()
+  app.run()
